bb5ef4653eb5611019621e032fe0e1a65ada911f
Joseph Karasek and Mary Warrington <joseph.karasek@gmail.com>
2016-02-10 13:23:37 -0800

    add initial project files and folders

---
+++ src/Job.php
@@ -0,0 +1,31 @@
+<?php
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Job.php';
+
+    session_start();
+    if (empty($_SESSION['list_of_jobs'])) {
+        $_SESSION['list_of_places'] = array();
+    }
+
+    $app = new Silex\Applicatoin();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    //routes
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
